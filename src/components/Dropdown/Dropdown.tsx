7821358157133eb4f04bc24daeed069b94242dda
khun <woghd567@naver.com>
2022-10-07 11:32:06 +0900

    REFACTOR: Dropdown useClickOutside Hook 적용

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import { ReactComponent as ArrowDownIcon } from '../../assets/icons/arrowDown.svg';
+
+import useClickOutside from '../../hooks/useClickOutside';
 
 import * as S from './Dropdown.styled';
 
@@ -9,9 +11,14 @@ import { IDropdownProps } from './Dropdown.type';
 const Dropdown = (props: IDropdownProps) => {
   const { name, value, candidates, onUpdateBlock } = props;
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const ref = useRef(null);
+
+  useClickOutside(ref, () => {
+    setIsOpen(false);
+  });
 
   return (
-    <S.DropdownWrapper onClick={() => setIsOpen((isOpen) => !isOpen)}>
+    <S.DropdownWrapper ref={ref} onClick={() => setIsOpen((isOpen) => !isOpen)}>
       <span>{`${name} / ${value ? value.name : ''}`} </span>
       <ArrowDownIcon />
       {isOpen && (
